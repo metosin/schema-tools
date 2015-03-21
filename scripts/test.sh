2fd5be909d5341c333229021f0d963ca0662a138
Juho Teperi <juho.teperi@iki.fi>
2015-03-21 15:26:00 +0200

    Cljs test fixes

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -e
 lein trampoline run -m clojure.main scripts/build.clj
 node scripts/runner.js
--- test/schema_tools/runner.cljs
+++ test/schema_tools/runner.cljs
@@ -1,11 +1,12 @@
 (ns schema-tools.runner
   (:require [cljs.test :as test]
             [cljs.nodejs :as nodejs]
-            schema-tools.core-test))
+            schema-tools.core-test
+            schema-tools.walk-test))
 
 (nodejs/enable-util-print!)
 
 (defn -main []
-  (test/run-all-tests))
+  (test/run-all-tests #"^schema-tools.*-test$"))
 
 (set! *main-cli-fn* -main)
