62e5e9efd508e85137d9982280248af1efae2f6b
Tommi Reiman <tommi@iki.fi>
2015-07-25 22:05:22 +0300

    0.5.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/schema-tools "0.4.3"
+(defproject metosin/schema-tools "0.5.0-SNAPSHOT"
   :description "Common utilities for Prismatic Schema"
   :url "https://github.com/metosin/schema-tools"
   :license {:name "Eclipse Public License"
