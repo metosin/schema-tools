c0863b29fa27790406d45a194559039f7223fffb
Tommi Reiman <tommi@iki.fi>
2015-08-05 08:28:38 +0300

    update clojurescript

--- project.clj
+++ project.clj
@@ -33,7 +33,7 @@
                              [jonase/eastwood "0.2.1"]]
                    :dependencies [[criterium "0.4.3"]
                                   [org.clojure/clojure "1.7.0"]
-                                  [org.clojure/clojurescript "0.0-3308"]]}
+                                  [org.clojure/clojurescript "1.7.28"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.7"]
             "test-clj" ["all" "do" ["cljx" "once"] ["test"] ["check"]]})
