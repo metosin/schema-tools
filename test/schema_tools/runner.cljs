19de55525f50a4a054eb497c43898ce787391f91
Juho Teperi <juho.teperi@iki.fi>
2015-09-06 18:41:22 +0300

    Set return code for cljs tests if tests fail

--- test/schema_tools/runner.cljs
+++ test/schema_tools/runner.cljs
@@ -9,7 +9,13 @@
 
 (nodejs/enable-util-print!)
 
+(def status (atom nil))
+
 (defn -main []
-  (test/run-all-tests #"^schema-tools.*-test$"))
+  (test/run-all-tests #"^schema-tools.*-test$")
+  (js/process.exit @status))
+
+(defmethod test/report [:cljs.test/default :end-run-tests] [m]
+  (reset! status (if (test/successful? m) 0 1)))
 
 (set! *main-cli-fn* -main)
