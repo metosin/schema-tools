7742fbb9003b9c4f9bfbef46d6d388fb10bc6256
Miikka Koskinen <miikka@metosin.fi>
2020-01-10 09:30:07 +0200

    Version 0.12.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/schema-tools "0.12.1-SNAPSHOT"
+(defproject metosin/schema-tools "0.12.1"
   :description "Common utilities for Prismatic Schema"
   :url "https://github.com/metosin/schema-tools"
   :license {:name "Eclipse Public License"
